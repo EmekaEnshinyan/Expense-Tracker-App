2119db8d1156fe122b95e8e03aad0f8256a14161
Enshinyan <emeka.enshinyan@gmail.com>
2022-07-11 01:49:16 -0700

    add: header

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 export default function Header(){
     return (
-        <div>
-            <h1>Emeka's Expense Tracker</h1>
-        </div>
+        <h1>Emeka's Expense Tracker App</h1>
     )
 }
-
